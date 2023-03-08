61712775198d85ea39daea23f6c8a4e2a017ff9b
Marcin Ciapa <aaryndo@gmail.com>
2023-03-08 19:42:23 +0100

    Solve problem 53a - manual cloning

---
+++ JavaCodingProblems/src/main/java/com/github/marcinciapa/chapter2/problem53/ManualShallowCopy.java
@@ -0,0 +1,30 @@
+package com.github.marcinciapa.chapter2.problem53;
+
+class ManualShallowCopy {
+
+    private double x;
+    private double y;
+
+    ManualShallowCopy() {
+    }
+
+    ManualShallowCopy(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    ManualShallowCopy copy() {
+        ManualShallowCopy copy = new ManualShallowCopy();
+        copy.x = this.x;
+        copy.y = this.y;
+        return copy;
+    }
+}
---
+++ JavaCodingProblems/src/test/java/com/github/marcinciapa/chapter2/problem53/ManualShallowCopyTest.java
@@ -0,0 +1,21 @@
+package com.github.marcinciapa.chapter2.problem53;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ManualShallowCopyTest {
+
+    @Test
+    void shouldCreateShallowCopyManually() {
+        // given
+        ManualShallowCopy original = new ManualShallowCopy(0.2, -0.5);
+
+        // when
+        ManualShallowCopy clone = original.copy();
+
+        // then
+        assertEquals(original.getX(), clone.getX());
+        assertEquals(original.getY(), clone.getY());
+    }
+}
