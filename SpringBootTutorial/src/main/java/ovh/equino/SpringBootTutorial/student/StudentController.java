e3ed6025b55610509e26cfe9b9f1ddc47738e852
Marcin Ciapa <aaryndo@gmail.com>
2023-07-31 23:43:05 +0200

    Complete spring boot tutorial

---
+++ SpringBootTutorial/src/main/java/ovh/equino/SpringBootTutorial/student/StudentController.java
@@ -0,0 +1,38 @@
+package ovh.equino.SpringBootTutorial.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerStudent(@RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, @RequestBody Student updatedStudent) {
+        studentService.updateStudent(studentId, updatedStudent);
+    }
+}
