460070da93f7486114c99e79a30e408244423a57
Marcin Ciapa <aaryndo@gmail.com>
2023-08-12 22:39:17 +0200

    Backend side complete

--- SpringBootAngularFullStackTutorial/server/src/main/java/com/github/marcinciapa/server/ServerApplication.java
+++ SpringBootAngularFullStackTutorial/server/src/main/java/com/github/marcinciapa/server/ServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import static com.github.marcinciapa.server.enumeration.Status.SERVER_DOWN;
 import static com.github.marcinciapa.server.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
@@ -20,10 +21,10 @@ public class ServerApplication {
 	@Bean
 	CommandLineRunner run(ServerRepo serverRepo) {
 		return args -> {
-			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
-			serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower", "http://localhost:8080/server/image/server2.png", SERVER_UP));
-			serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
-			serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.0.10", "Vienna", "8 GB", "Asus", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.0.58", "Fedora Linux", "16 GB", "Dell Tower", "http://localhost:8080/server/image/server2.png", SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.0.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.0.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
 		};
 	}
 }
