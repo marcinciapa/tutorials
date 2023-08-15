513b19efc3aa30bc30322acdd0daf794281202a5
Marcin Ciapa <aaryndo@gmail.com>
2023-08-15 12:26:27 +0200

    Generate and use JOOQ code

---
+++ SpringBootJooqTutorial/src/test/java/com/github/marcinciapa/SpringBootJooqTutorial/SpringBootJooqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.SpringBootJooqTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJooqTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
