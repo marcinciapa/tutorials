3cc6a1b70d707b24e32d51cd39526910a0eeb0d5
Marcin Ciapa <aaryndo@gmail.com>
2023-08-15 10:41:28 +0200

    Finish course without exercise

---
+++ SpringBootJdbcFlywayTutorial/src/main/java/com/github/marcinciapa/SpringBootJdbcFlywayTutorial/SpringBootJdbcFlywayTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.SpringBootJdbcFlywayTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcFlywayTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcFlywayTutorialApplication.class, args);
+	}
+
+}
---
+++ SpringBootJdbcFlywayTutorial/src/main/java/com/github/marcinciapa/SpringBootJdbcFlywayTutorial/actor/Actor.java
@@ -0,0 +1,4 @@
+package com.github.marcinciapa.SpringBootJdbcFlywayTutorial.actor;
+
+public record Actor(Integer id, String name) {
+}
