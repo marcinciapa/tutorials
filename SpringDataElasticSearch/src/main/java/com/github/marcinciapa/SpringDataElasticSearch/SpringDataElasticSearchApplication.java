cdd52c3875276480d742c5f7bedeca9458f4f387
Marcin Ciapa <aaryndo@gmail.com>
2023-09-30 09:23:00 +0200

    Initial project structure for SpringDataElasticSearch

---
+++ SpringDataElasticSearch/src/main/java/com/github/marcinciapa/SpringDataElasticSearch/SpringDataElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.SpringDataElasticSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataElasticSearchApplication.class, args);
+	}
+
+}
