bd773e8844121d4a3e58a0f70ab69656f019869f
Marcin Ciapa <aaryndo@gmail.com>
2023-10-14 11:13:31 +0200

    Lesson 4: Fetching running timers

--- SpringBootQuartzTutorial/src/main/java/com/github/marcinciapa/SpringBootQuartzTutorial/timerservice/SchedulerService.java
+++ SpringBootQuartzTutorial/src/main/java/com/github/marcinciapa/SpringBootQuartzTutorial/timerservice/SchedulerService.java
@@ -2,10 +2,8 @@ package com.github.marcinciapa.SpringBootQuartzTutorial.timerservice;
 
 import com.github.marcinciapa.SpringBootQuartzTutorial.info.TimerInfo;
 import com.github.marcinciapa.SpringBootQuartzTutorial.util.TimerUtils;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
+import org.quartz.*;
+import org.quartz.impl.matchers.GroupMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,10 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class SchedulerService {
@@ -34,6 +36,42 @@ public class SchedulerService {
             scheduler.scheduleJob(jobDetail, trigger);
         } catch (SchedulerException e) {
             LOG.error(e.getMessage(), e);
+        }
+    }
+
+    public List<TimerInfo> getAllRunningTimers() {
+        try {
+            return scheduler.getJobKeys(GroupMatcher.anyGroup())
+                    .stream()
+                    .map(jobKey -> {
+                        try {
+                            final JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+                            return (TimerInfo) jobDetail.getJobDataMap().get(jobKey.getName());
+                        } catch (SchedulerException e) {
+                            LOG.error(e.getMessage(), e);
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        } catch (SchedulerException e) {
+            LOG.error(e.getMessage(), e);
+            return Collections.emptyList();
+        }
+    }
+
+    public TimerInfo getRunningTimer(String timerId) {
+        try {
+            final JobDetail jobDetail = scheduler.getJobDetail(new JobKey(timerId));
+
+            if(jobDetail == null) {
+                return null;
+            }
+            
+            return (TimerInfo) jobDetail.getJobDataMap().get(timerId);
+        } catch (SchedulerException e) {
+            LOG.error(e.getMessage(), e);
+            return null;
         }
     }
 
