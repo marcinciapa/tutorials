aafc10f8d21ff71bfb63279c8dad127a1abaf386
Marcin Ciapa <aaryndo@gmail.com>
2023-10-14 11:56:47 +0200

    Lesson 5: updating and deleting timer jobs

--- SpringBootQuartzTutorial/src/main/java/com/github/marcinciapa/SpringBootQuartzTutorial/jobs/HelloWorldJob.java
+++ SpringBootQuartzTutorial/src/main/java/com/github/marcinciapa/SpringBootQuartzTutorial/jobs/HelloWorldJob.java
@@ -4,7 +4,6 @@ import com.github.marcinciapa.SpringBootQuartzTutorial.info.TimerInfo;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,6 +17,6 @@ public class HelloWorldJob implements Job {
     public void execute(JobExecutionContext context) {
         JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
         TimerInfo info = (TimerInfo) jobDataMap.get(HelloWorldJob.class.getSimpleName());
-        LOG.info(info.getCallbackData());
+        LOG.info("Remaining fire count is {}", info.getRemainingFireCount());
     }
 }
--- SpringBootQuartzTutorial/src/main/java/com/github/marcinciapa/SpringBootQuartzTutorial/timerservice/SchedulerService.java
+++ SpringBootQuartzTutorial/src/main/java/com/github/marcinciapa/SpringBootQuartzTutorial/timerservice/SchedulerService.java
@@ -60,14 +60,15 @@ public class SchedulerService {
         }
     }
 
-    public TimerInfo getRunningTimer(String timerId) {
+    public TimerInfo getRunningTimer(final String timerId) {
         try {
             final JobDetail jobDetail = scheduler.getJobDetail(new JobKey(timerId));
 
-            if(jobDetail == null) {
+            if (jobDetail == null) {
+                LOG.error("Failed to find timer with ID '{}'", timerId);
                 return null;
             }
-            
+
             return (TimerInfo) jobDetail.getJobDataMap().get(timerId);
         } catch (SchedulerException e) {
             LOG.error(e.getMessage(), e);
@@ -75,10 +76,35 @@ public class SchedulerService {
         }
     }
 
+    public void updateTimer(final String timerId, final TimerInfo info) {
+        try {
+            final JobDetail jobDetail = scheduler.getJobDetail(new JobKey(timerId));
+
+            if (jobDetail == null) {
+                LOG.error("Failed to find timer with ID '{}'", timerId);
+                return;
+            }
+
+            jobDetail.getJobDataMap().put(timerId, info);
+        } catch (SchedulerException e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
+
+    public Boolean deleteTimer(final String timerId) {
+        try {
+            return scheduler.deleteJob(new JobKey(timerId));
+        } catch (SchedulerException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return false;
+    }
+
     @PostConstruct
     public void init() {
         try {
             scheduler.start();
+            scheduler.getListenerManager().addTriggerListener(new SimpleTriggerListener(this));
         } catch (SchedulerException e) {
             LOG.error(e.getMessage(), e);
         }
