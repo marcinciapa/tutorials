fac00c73b908081d751d9b4b0c945fe1309b0e2a
Marcin Ciapa <aaryndo@gmail.com>
2023-10-15 11:51:01 +0200

    Spring boot quartz tutorial (#7)
    * Prepare file structure
    
    * Lesson 1 - starting and stopping Quartz scheduler
    
    * Lesson2: Configuring job and trigger
    
    * Lesson 3: scheduling a job
    
    * Lesson 4: Fetching running timers
    
    * Lesson 5: updating and deleting timer jobs
    
    * Persist Quartz jobs in MySQL database
    
    * Update README formatting

---
+++ SpringBootQuartzTutorial/src/test/java/com/github/marcinciapa/SpringBootQuartzTutorial/SpringBootQuartzTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.SpringBootQuartzTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuartzTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
