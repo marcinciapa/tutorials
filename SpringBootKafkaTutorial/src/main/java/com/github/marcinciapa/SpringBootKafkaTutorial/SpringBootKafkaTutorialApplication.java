f02978ad67af26a32b929c7ba13836d5a720fd2e
Marcin Ciapa <aaryndo@gmail.com>
2023-11-18 13:12:30 +0100

    Produce hello message

--- SpringBootKafkaTutorial/src/main/java/com/github/marcinciapa/SpringBootKafkaTutorial/SpringBootKafkaTutorialApplication.java
+++ SpringBootKafkaTutorial/src/main/java/com/github/marcinciapa/SpringBootKafkaTutorial/SpringBootKafkaTutorialApplication.java
@@ -1,7 +1,10 @@
 package com.github.marcinciapa.SpringBootKafkaTutorial;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringBootKafkaTutorialApplication {
@@ -10,4 +13,10 @@ public class SpringBootKafkaTutorialApplication {
 		SpringApplication.run(SpringBootKafkaTutorialApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("amigoscode", "hello kafka");
+		};
+	}
 }
