5f42b49d0f6ef3a8239eeaa306548426bfbd45da
Marcin Ciapa <aaryndo@gmail.com>
2023-11-30 07:43:44 +0100

    SpringBoot Kafka tutorial (#8)
    * Initialize SpringBoot project
    
    * Start application with Kafka topic
    
    * Produce hello message
    
    * Create topic consumer
    
    * Expose an API for publishing messages

---
+++ SpringBootKafkaTutorial/src/main/java/com/github/marcinciapa/SpringBootKafkaTutorial/KafkaListeners.java
@@ -0,0 +1,14 @@
+package com.github.marcinciapa.SpringBootKafkaTutorial;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(topics = "amigoscode", groupId = "groupId")
+    void listener(String data) {
+        System.out.println("Listener received: " + data + " :tada:");
+    }
+
+}
---
+++ SpringBootKafkaTutorial/src/test/java/com/github/marcinciapa/SpringBootKafkaTutorial/SpringBootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.SpringBootKafkaTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
