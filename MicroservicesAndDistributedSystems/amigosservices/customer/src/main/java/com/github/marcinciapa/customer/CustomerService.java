bc7245c086ac96272c7139fe2db8fad4cff15746
Marcin Ciapa <aaryndo@gmail.com>
2024-01-05 12:24:29 +0100

    Your first microservice (#11)

---
+++ MicroservicesAndDistributedSystems/amigosservices/customer/src/main/java/com/github/marcinciapa/customer/CustomerService.java
@@ -0,0 +1,15 @@
+package com.github.marcinciapa.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        customerRepository.save(customer);
+    }
+}
