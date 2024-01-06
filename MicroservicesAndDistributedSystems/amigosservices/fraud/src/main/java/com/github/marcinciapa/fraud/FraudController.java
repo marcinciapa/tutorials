004b8b29b78ac6ab85507d5b822b2d0c15b176da
Marcin Ciapa <aaryndo@gmail.com>
2024-01-06 16:30:16 +0100

    Microservices and Distributed Systems - Open Feign (#14)

--- MicroservicesAndDistributedSystems/amigosservices/fraud/src/main/java/com/github/marcinciapa/fraud/FraudController.java
+++ MicroservicesAndDistributedSystems/amigosservices/fraud/src/main/java/com/github/marcinciapa/fraud/FraudController.java
@@ -1,5 +1,6 @@
 package com.github.marcinciapa.fraud;
 
+import com.github.marcinciapa.clients.FraudCheckResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
