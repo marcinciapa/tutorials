e29dbfec316395dc6c77b095b9c5f9387f3acc1c
Marcin Ciapa <aaryndo@gmail.com>
2024-02-03 20:40:44 +0100

    Implement JwtAuthFilter

---
+++ SpringSecurityTutorial/src/main/java/com/github/marcinciapa/tutorials/security/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.github.marcinciapa.tutorials.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+        return http.build();
+    }
+}
---
+++ SpringSecurityTutorial/src/test/java/com/github/marcinciapa/tutorials/security/SpringSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.tutorials.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
