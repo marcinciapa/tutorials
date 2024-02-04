7c36a0d4c72a5aaf81939486fda9f50b4f678248
Marcin Ciapa <aaryndo@gmail.com>
2024-02-04 09:04:35 +0100

    Spring Security tutorial (#32)
    * Implement JwtAuthFilter
    
    * Finish Spring Security course

---
+++ SpringSecurityTutorial/src/main/java/com/github/marcinciapa/tutorials/security/config/dao/UserDao.java
@@ -0,0 +1,28 @@
+package com.github.marcinciapa.tutorials.security.config.dao;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User("mail@gmail.com", "password", Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+            new User("liam@gmail.com", "password", Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")))
+    );
+
+    public UserDetails findUserByEmail(String email) {
+        return APPLICATION_USERS
+                .stream()
+                .filter(u -> u.getUsername().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
+    }
+}
