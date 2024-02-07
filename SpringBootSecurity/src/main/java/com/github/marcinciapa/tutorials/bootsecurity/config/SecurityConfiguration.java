f3d79fae7faae3e01e7c684fcb6a6116d2f6aa17
Marcin Ciapa <aaryndo@gmail.com>
2024-02-07 20:13:04 +0100

    Implement demo endpoints and test

--- SpringBootSecurity/src/main/java/com/github/marcinciapa/tutorials/bootsecurity/config/SecurityConfiguration.java
+++ SpringBootSecurity/src/main/java/com/github/marcinciapa/tutorials/bootsecurity/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
