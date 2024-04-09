9278f79d60c952050d404d95044395b874e03d10
Marcin Ciapa <aaryndo@gmail.com>
2024-04-09 21:18:34 +0200

    Getting started with Python - Working with dates

---
+++ GettingStartedWithPython/my-python-app/08_dates/working_with_dates.py
@@ -0,0 +1,16 @@
+import datetime
+
+print(datetime.datetime.now())  # current date time
+print(datetime.date.today())  # current date
+print(datetime.datetime.now().time())  # current time
+
+from datetime import datetime
+from datetime import date
+
+print(datetime.now())  # current date time
+print(date.today())  # current date
+print(datetime.now().date())  # current date
+print(datetime.now().time())  # current time
+print(datetime.now().year)  # current year
+print(datetime.now().month)  # current month
+print(datetime.now().day)  # current day of month
