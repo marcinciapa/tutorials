42230e43e9d0e6c0ae503dd11100a315d47c26b8
Marcin Ciapa <aaryndo@gmail.com>
2024-04-18 22:21:11 +0200

    Professional Full Stack Developer - CRUD - Read (#50)

--- ProfessionalFullStackDeveloper/spring-boot-example/src/main/java/com/github/marcinciapa/tutorials/fullstack/Main.java
+++ ProfessionalFullStackDeveloper/spring-boot-example/src/main/java/com/github/marcinciapa/tutorials/fullstack/Main.java
@@ -3,73 +3,116 @@ package com.github.marcinciapa.tutorials.fullstack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.github.marcinciapa.tutorials.fullstack")
-//@EnableAutoConfiguration
-//@Configuration
-
 @RestController
 public class Main {
+
+    private static List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+        Customer alex = new Customer(1, "Alex", "alex@gmail.com", 21);
+        Customer jamila = new Customer(2, "Jamila", "jamila@gmail.com", 19);
+        customers.add(alex);
+        customers.add(jamila);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet(
-            @RequestParam(value = "name", required = false) String name) {
-
-        String greetMessage = name == null || name.isBlank() ? "Hello" : "Hello %s".formatted(name);
-        GreetResponse response = new GreetResponse(
-                greetMessage,
-                List.of("Java", "Python", "Kotlin"),
-                new Person("Marcin", 22, 30_000)
-        );
-        return response;
+    //    @RequestMapping(path = "api/v1/customers", method = RequestMethod.GET)
+    @GetMapping("api/v1/customers")
+    public List<Customer> getCustomers() {
+        return customers;
     }
 
-    record GreetResponse(String greet,
-                         List<String> favProgrammingLanguages,
-                         Person person) {
+    @GetMapping("api/v1/customers/{customerId}")
+    public Customer getCustomer(@PathVariable("customerId") Integer customerId) {
+        Customer customer = customers
+                .stream()
+                .filter(c -> customerId.equals(c.id))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("customer with id %s not found".formatted(customerId)));
+        return customer;
     }
 
-    record Person(String name, int age, double savings) {
-    }
+    static class Customer {
+        private Integer id;
+        private String name;
+        private String email;
+        private Integer age;
 
-//    class GreetResponse {
-//        private final String greet;
-//
-//        GreetResponse(String greet) {
-//            this.greet = greet;
-//        }
-//
-//        public String getGreet() {
-//            return greet;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' +
-//                    '}';
-//        }
-//
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            GreetResponse that = (GreetResponse) o;
-//            return Objects.equals(greet, that.greet);
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return Objects.hashCode(greet);
-//        }
-//    }
+        public Customer() {
+        }
+
+        public Customer(Integer id, String name, String email, Integer age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Customer customer = (Customer) o;
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
 }
