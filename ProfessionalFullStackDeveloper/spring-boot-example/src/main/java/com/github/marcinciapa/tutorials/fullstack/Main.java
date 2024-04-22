eb479d7843f6d6397479d0ca7b76be8ca2f148b1
Marcin Ciapa <aaryndo@gmail.com>
2024-04-22 08:51:07 +0200

    Professional Full Stack Developer - Error Handling (#53)

--- ProfessionalFullStackDeveloper/spring-boot-example/src/main/java/com/github/marcinciapa/tutorials/fullstack/Main.java
+++ ProfessionalFullStackDeveloper/spring-boot-example/src/main/java/com/github/marcinciapa/tutorials/fullstack/Main.java
@@ -2,9 +2,7 @@ package com.github.marcinciapa.tutorials.fullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,22 +11,5 @@ public class Main {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
-
-        printBeans(applicationContext);
-    }
-
-    @Bean("foo")
-    Foo getFoo() {
-        return new Foo("bar");
-    }
-
-    public record Foo(String name) {
-    }
-
-    private static void printBeans(ApplicationContext ctx) {
-        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
-        }
     }
 }
