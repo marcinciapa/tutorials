96879d24d1d40a76704027b03e8b9438a479362c
Marcin Ciapa <aaryndo@gmail.com>
2024-06-09 18:50:12 +0200

    Professional Fullstack Developer - Integration testing - pt3

--- ProfessionalFullStackDeveloper/spring-boot-example/src/test/java/com/github/marcinciapa/tutorials/fullstack/journey/CustomerIntegrationTest.java
+++ ProfessionalFullStackDeveloper/spring-boot-example/src/test/java/com/github/marcinciapa/tutorials/fullstack/journey/CustomerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
 import com.github.marcinciapa.tutorials.fullstack.customer.Customer;
 import com.github.marcinciapa.tutorials.fullstack.customer.CustomerRegistrationRequest;
+import com.github.marcinciapa.tutorials.fullstack.customer.CustomerUpdateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +18,6 @@ import java.util.Random;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -148,6 +148,69 @@ public class CustomerIntegrationTest {
 
     @Test
     void canUpdateCustomer() {
-        fail("not implemented");
+        // create registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + "-" + UUID.randomUUID() + "@foobarhello123.com";
+        int age = RANDOM.nextInt(1, 100);
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, age);
+
+        // send a post request
+        webTestClient.post()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        // get all customers
+        List<Customer> allCustomers = webTestClient.get()
+                .uri(CUSTOMER_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(new ParameterizedTypeReference<Customer>() {
+                })
+                .returnResult()
+                .getResponseBody();
+
+        var id = allCustomers
+                .stream()
+                .filter(customer -> customer.getEmail().equals(email))
+                .map(Customer::getId)
+                .findFirst()
+                .orElseThrow();
+
+        // create update request
+        Name newFakerName = faker.name();
+        String newName = newFakerName.fullName();
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(newName, null, null);
+
+        Customer expectedCustomer = new Customer(id, newName, email, age);
+
+        // send a put request
+        webTestClient.put()
+                .uri(CUSTOMER_URI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(updateRequest), CustomerUpdateRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        // get customer by id
+        webTestClient.get()
+                .uri(CUSTOMER_URI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(new ParameterizedTypeReference<Customer>() {
+                })
+                .isEqualTo(expectedCustomer);
     }
 }
