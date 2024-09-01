7a6a9d2efa7744dc6c741744237c6433c6293eee
Marcin Ciapa <aaryndo@gmail.com>
2024-09-01 12:10:04 +0200

    Spring Boot GraphQL tutorial

---
+++ SpringBootGraphQL/src/test/java/com/github/marcinciapa/tutorials/springbootgraphql/SpringBootGraphQL/SpringBootGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.tutorials.springbootgraphql.SpringBootGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
