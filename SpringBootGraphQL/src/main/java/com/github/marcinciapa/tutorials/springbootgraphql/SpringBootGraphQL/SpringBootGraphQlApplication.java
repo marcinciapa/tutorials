9d43984d31e34eeba3245dbf5bd662ae5efe4511
Marcin Ciapa <aaryndo@gmail.com>
2024-09-04 19:22:37 +0200

    Spring Boot GraphQL tutorial (#74)
    * Spring Boot GraphQL tutorial
    
    * Fix typo

---
+++ SpringBootGraphQL/src/main/java/com/github/marcinciapa/tutorials/springbootgraphql/SpringBootGraphQL/SpringBootGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcinciapa.tutorials.springbootgraphql.SpringBootGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphQlApplication.class, args);
+	}
+
+}
